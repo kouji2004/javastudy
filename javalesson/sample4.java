95692a9d9facbfa743e42f1b11fff5a88028f470
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-02-08 16:20:26 +0900

    変数について

---
+++ javalesson/sample4.java
@@ -0,0 +1,11 @@
+package javalesson;
+
+public class sample4 {
+  public static void main(String[] args) {
+    //変数の使い方
+    System.out.println(3*3*3.14);
+    int r;
+    r =3;
+    System.out.println(r*r*3.14);
+  }
+}
