449b0e9013d2895626705072ff5da2f4be646689
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-02-08 16:13:31 +0900

    文字列と数値の組み合わせ

--- javalesson/sample3.java
+++ javalesson/sample3.java
@@ -2,6 +2,11 @@ package javalesson;
 
 public class sample3 {
   public static void main(String[] args) {
+    //数値について
     System.out.println(10);
+
+    //文字列と数値の組み合わせ
+    System.out.println("計算"+(10+5));
+    System.out.println(1000+"円");
   }
 }
