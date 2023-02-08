c3364bb4419c9136e9f7f81a723c357dadd28de8
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-02-08 16:16:58 +0900

    小数の扱いかた

--- javalesson/sample3.java
+++ javalesson/sample3.java
@@ -8,5 +8,14 @@ public class sample3 {
     //文字列と数値の組み合わせ
     System.out.println("計算"+(10+5));
     System.out.println(1000+"円");
+
+    //四則演算時(足し算、引き算、掛け算、割り算のこと)
+    System.out.println(10+4);
+    System.out.println(10-4);
+    System.out.println(10*4);
+    System.out.println(10/4);
+
+    //小数の扱い方
+    System.out.println(3.14);
   }
 }
