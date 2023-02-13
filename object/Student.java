5b89eabec33ecb45008023a99a4790043aa03603
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-02-13 14:04:08 +0900

    設計図の作成

--- object/Student.java
+++ object/Student.java
@@ -1,5 +1,23 @@
 package object;
 
 public class Student {
-  
+  //属性（変数）
+  String name;
+  int engScore;
+  int mathScore;
+
+  //操作(メソッド)
+  void display(){
+    System.out.println(name+"さん");
+    System.out.println("英語"+engScore+"点・数学"+mathScore+"点");
+  }
+  void setScore(int eng, int math){
+    engScore = eng;
+    mathScore = math;
+  }
+
+  double getAvg(){
+    double avg = (engScore+mathScore)/2.0;
+    return avg;
+  }
 }
